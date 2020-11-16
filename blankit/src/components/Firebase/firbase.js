3577937e22ef6f47d85076d109780a92852b5f5e
diana maina <dianamainachui@gmail.com>
2020-11-16 06:01:52 -0500

    added firebase dependancies

---
+++ blankit/src/components/Firebase/firbase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAbl0iy3N5J_za1ngNYxMkkqQpJMhdwABw",
+    authDomain: "blankit-34420.firebaseapp.com",
+    databaseURL: "https://blankit-34420.firebaseio.com",
+    projectId: "blankit-34420",
+    storageBucket: "blankit-34420.appspot.com",
+    messagingSenderId: "943184230774",
+    appId: "1:943184230774:web:53e17b435d583313e06257"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
